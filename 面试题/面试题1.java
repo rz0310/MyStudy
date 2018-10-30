bb3dae6a6e522a618e6bd375df84dbae1d12cfd2
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-30 22:54:01 +0800

    面试题
    面试题

---
+++ 面试题/面试题1.java
@@ -0,0 +1,8 @@
+package 练习;
+
+class 面试题1 {
+	int age = 2;
+	String name;
+	// 赋值语句,不能直接放在类中
+//	name = "fkit";
+}
---
+++ 面试题/面试题2.java
@@ -0,0 +1,10 @@
+package 练习;
+
+public class 面试题2 {
+	int age = 2;
+	String name;
+	// 初始化块
+	{
+		name = "fkit";
+	}
+}
