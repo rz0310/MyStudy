e128b87b5664927d71b46cecdf19327528f08713
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-11-04 19:56:47 +0800

    PersonTest
    PersonTest

---
+++ 验证/PersonTest.java
@@ -0,0 +1,12 @@
+package ¿ÎÌÃÁ·Ï°;
+
+public class PersonTest {
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setName("renjing");
+		System.out.println(p.getName());
+//		System.out.println(p.getAge());
+		p.setAge(25);
+		System.out.println(p.getAge());
+	}
+}
