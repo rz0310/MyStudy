0462369c95169a780041745bdb0e299c3df16c8e
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-11-06 22:26:38 +0800

    BirdTest
    BirdTest

---
+++ 继承/BirdTest.java
@@ -0,0 +1,17 @@
+package ¿ÎÌÃÁ·Ï°;
+
+public class BirdTest {
+
+	public static void main(String[] args) {
+		Bird b = new Bird();
+		b.fly();
+		b.name = "Äñ";
+		
+		Sparrow sp = new Sparrow();
+		sp.fly();
+		
+		Ostrich o = new Ostrich();
+		o.fly();
+	}
+
+}
