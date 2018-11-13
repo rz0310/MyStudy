ab0dde2affa398968198bc4e383064b7ec32f6c6
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-11-13 22:44:10 +0800

    Apple
    Apple

---
+++ ToString和HashCode/Apple.java
@@ -0,0 +1,36 @@
+package 练习;
+
+public class Apple {
+	private String color;
+	private double weight;
+	public Apple(){
+		
+	}
+	public Apple(String color,double weight){
+		this.color = color;
+		this.weight = weight;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	@Override
+	public int hashCode(){
+		return 20;
+	}
+	@Override
+	public String toString(){
+		return "Apple[color="+color+",weight="+weight
+//				列出所有的成员变量
+				+"]";
+		
+	}
+}
