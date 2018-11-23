5fe9f9a44642860e3bd1e76a9838006bf1b61101
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-11-23 23:33:26 +0800

    2
    2

---
+++ 面试题2/面试题2.java
@@ -0,0 +1,13 @@
+package 练习;
+
+public class 面试题2 {
+//	非静态方法
+	public void info(){
+		System.out.println("info方法");
+	}
+//	静态方法
+	public static void main(String[] args) {
+//		在静态方法中调用非静态方法：编译报错
+//		info();
+	}
+}
