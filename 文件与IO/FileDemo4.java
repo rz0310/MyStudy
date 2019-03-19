2ef59edf855d2e23323e2d76ee680784f680abc7
rz0310 <43040390+rz0310@users.noreply.github.com>
2019-03-19 22:15:35 +0800

    FileDemo4
    FileDemo4

---
+++ 文件与IO/FileDemo4.java
@@ -0,0 +1,14 @@
+package 文件与IO;
+
+import java.io.File;
+
+public class FileDemo4 {
+
+   public static void main(String[] args){
+       //    创建文件
+       //    方法1
+       File f1 = new File("E:\\FileTest\\a.txt");
+       File f2 = new File("E:/FileTest/b.txt");
+
+   }
+}
